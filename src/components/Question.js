a7398f3e349370f380a7b01d578c17b9cf397752
Charbel Maroun <charbel-maroun@hotmail.com>
2023-01-15 13:07:02 -0500

    commit tree 2914195493a4f5d99f57ee5b4f53f22bcfc6b10d

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
@@ -9,6 +9,27 @@ function Question({ question, onAnswered }) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
+  useEffect(() => {
+
+    const timerId = setTimeout(() => {
+      setTimeRemaining(timeRemaining => timeRemaining - 1)
+    }, 1000)
+
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+
+    }
+
+    return function () {
+      clearTimeout(timerId)
+    }
+
+
+  }, [timeRemaining, onAnswered])
+
 
   const { id, prompt, answers, correctIndex } = question;
 
